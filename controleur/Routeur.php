d0308cab9c63ba4922cee84b782d1f34508b5248
tensaiji <47110246+tensaiji@users.noreply.github.com>
2021-10-13 11:24:11 +0200

    ninja
    + possibilite de rediriger sur l'index tout en indiquant une erreur (?err=<code>)
    + les dossiers vue, modele et controlleur redirige automatiquement vers la page 404

--- controleur/Routeur.php
+++ controleur/Routeur.php
@@ -3,11 +3,15 @@
 
     class Routeur
     {
+        const INDICATEUR_ERREUR = 'err';
+
         static function _definir_vue(array $requete) : string
         {
             if (count($requete) === 0)
                 return '';
-            return key($requete);
+            $vue = key($requete);
+            if ($vue === self::INDICATEUR_ERREUR)
+                return $vue . '/' . $requete[$vue];
         }
 
         static function charger_page(array $requete)
