2833a01128d1c65885697c682c02911263ed2439
tensaiji <47110246+tensaiji@users.noreply.github.com>
2021-10-14 18:00:59 +0200

    ini

--- controleur/Routeur.php
+++ controleur/Routeur.php
@@ -4,7 +4,7 @@
     class Routeur extends Modele
     {
         public function informations(): array
-        { return ['index', 'ierr']; }
+        { return ['index', 'strict', 'ierr']; }
         private const INI = 'ini/routeur.ini';
 
         public function __construct(?string $fichier_ini = null)
@@ -15,18 +15,27 @@
         
         protected $_index;                          // Nom de la vue par défaut
         public function index() : string { return $this->_index; }
+        protected $_strict;                          // Si vrai, seule les requêtes de la forme */<vue>/ seront acceptées
+        public function strict() : bool { return $this->_strict; }
+        protected function modifier_strict(bool $valeur) { $this->_strict = $valeur; }
         protected $_ierr;                           // Clé d'indication du code erreur
         public function ierr() : string { return $this->_ierr; }
      
-        public function definir_vue(array $requete) : string
+        public function definir_vue(string $uri) : string
         {
-            if (count($requete) === 0)              // La requête est vide
-                return $this->_index;                // On chargera la vue par défaut
-                
-            $vue = key($requete);                   // Récupération du nom de la vue (premiere clé du tableau de la requête)
-            if ($vue === $this->_ierr)   // Spécification d'une erreur
-                return $vue . '/' . $requete[$vue];
-            return $vue;
+            $uri = substr($uri, 1);                 // On retire le premier '/'
+            if ($uri[0] == '?' 
+            && (strpos($uri, '+', 1) < strpos($uri, '=', 1))
+            && $this->_strict)
+                $uri = $this->_ierr . '=404';           // 404
+            if (strlen($uri) === 0)                 // La requête est vide
+                return $this->_index;                  // On chargera la vue par défaut
+            if ($uri[-1] == '/')             // Adaptation des requêtes de la forme */<vue>/
+                $uri = substr($uri, 0, -1);             // On retire le dernier '/'
+            $err = explode('=', $uri, 2);
+            if ($err[0] === $this->_ierr)   // Spécification d'une erreur
+                return implode('/', $err);
+            return $uri;
         }
     }
 
--- index.php
+++ index.php
@@ -5,15 +5,15 @@
 
     $bd = new BD();                                                             // Initialise la base de données
     $auth = new Authentification();
-    $jeton_auth = new JetonAuthentification();                                        // Non authentifié par défaut
+    $jeton_auth = null;                                                         // Non authentifié par défaut
 
     $routeur = new Routeur();
-    $vue = $routeur->definir_vue($_REQUEST);                                     // Définir la vue
+    $vue = $routeur->definir_vue($_SERVER['REQUEST_URI']);                                     // Définir la vue
 
     if ($auth->obligatoire() || $auth->est_requise($vue))                       // Si l'authentification est requise
     {
         $jeton_auth = $auth->jeton($_SESSION, $bd);                        // Vérifie l'authentification
-        if (!$jeton_auth)                                                             // Si elle échoue
+        if (!$jeton_auth->valide())                                                             // Si elle échoue
             $vue = $auth->vue();                                           // Définir la vue sur la page de connexion
     }
     Vues::charger($vue, $jeton_auth);                                                 // Charger la vue
